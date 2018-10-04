74f28b3b3368e2cd5c598559ad6dd51ad6b35b46
Jordan Salisbury <salisburyjordan21@gmail.com>
2018-10-03 18:55:08 -0700

    bank account is now correctly working

--- script.js
+++ script.js
@@ -7,52 +7,60 @@
 // call that funtion 3 times in its own variable 
 $(document).ready(initializeApp);
 
-function initializeApp(){
+function initializeApp() {
   $('.spinButton').on('click', randomNum);
 
+  $('.bankAccount').text(bankAccountBalance);
 };
 
 
-
-$('<div>').addClass('.bus')
 var num1 = null;
 var num2 = null;
 var num3 = null;
 var newArray = [];
-var playerScore = 1500;
-var bankAccountBalance= 0
+// var playerScore = 1500;
+var bankAccountBalance = 8000;
 
-function randomNum(){
-  
+function randomNum() {
+
   num1 = Math.floor(Math.random() * 8);
   num2 = Math.floor(Math.random() * 8);
   num3 = Math.floor(Math.random() * 8);
-  newArray.push(num1,num2,num3);
+  newArray.push(num1, num2, num3);
   console.log(newArray);
-  
+
   displayNumbers(newArray);
 
-  if(num1===7 && num2===7 && num3===7){
+  if (num1 === 7 && num2 === 7 && num3 === 7) {
     console.log('user was successful');
     alert('congrats!');
-    bankAccountBalance +=5000;
-  } else if (num1 !== 7 && num2 !==7 && num3 !==7){
+    bankAccountBalance += 5000;
+    
+  } else if (num1 !== 7 && num2 !== 7 && num3 !== 7) {
     console.log('user failed');
-    alert('sorry try again');
+    // alert('sorry try again');
     bankAccountBalance -= 50;
-  } 
-  
-  newArray.length=0;
-  $('.balance').text(bankAccount);
+    
+    console.log(bankAccountBalance);
+  }
+
+
+  $('.bankAccount').text(bankAccountBalance);
+  newArray.length = 0;
 
 };
 
-function displayNumbers(newArray){
+function displayNumbers(newArray) {
   $(".output1").text(num1);
   $(".output2").text(num2);
   $(".output3").text(num3);
-  
+
 }
+
+
+
+
+
 
 
 
